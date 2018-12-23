4020b63d4800c6885e21ca58b1790b0f663ea6a6
Marc <envdia@gmail.com>
2018-12-23 14:05:55 -0600

    errors are handled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Flashcard from './components/flashcard'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<BrowserRouter>
+    <Switch>
+    <Route path="/:id" component={Flashcard} />
+    <Route path="/" component={Flashcard} />
+    </Switch>
+</BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
